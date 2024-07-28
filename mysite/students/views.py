faa1c6aebcd02fba0f78f047072219fec8043851
Noha Shehab <63148220+NohaAShehab@users.noreply.github.com>
2024-07-28 11:29:12 +0300

    intro to django

---
+++ mysite/students/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+# functions
+# handle http request?
+# system --> students and courses and tracks
+
+def itiview(request):
+    print(request)
+    #
+    return HttpResponse("<h1 style='color:red'>iti</h1>")
