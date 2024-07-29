20b6dd281d286e86651d3728511d14234f19089d
Noha Shehab <63148220+NohaAShehab@users.noreply.github.com>
2024-07-29 11:51:34 +0300

    using models

--- mysite/students/admin.py
+++ mysite/students/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+
+# each app you have ==> contains file admin ??
+# you can add your models here
 # Register your models here.
+from students.models import Student
+admin.site.register(Student)
--- mysite/students/models.py
+++ mysite/students/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+# create new model => descript students
+# id ,name, track, image, email, grade
+
+class Student(models.Model):
+    # django models ->provides ? datatypes you can use while definning
+    # the columns
+    # null property works on db level, blank works on admin dashboard level
+    name =models.CharField(max_length=100)
+    image= models.CharField(max_length=100, null=True)
+    track = models.CharField(max_length=100, null=True, blank=True)
+    email = models.EmailField(max_length=100, null=True, blank=True, unique=True)
+    grade = models.IntegerField(default=0, null=True, blank=True)
+
+
+    def __str__(self):
+        return self.name
+
+
